e0e37c47d68079039c30a2ca937f91010eb4f816
BRReed <b.r.reed@gmail.com>
2022-09-10 14:29:12 -0700

    added method declarations
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- Headers/ChessRules.hpp
+++ Headers/ChessRules.hpp
@@ -1,5 +1,8 @@
 #ifndef CHESSRULES_HPP
 #define CHESSRULES_HPP
+#include<string>
+#include<vector>
+#include<unordered_map>
 
 class ChessRules
 {
@@ -8,7 +11,14 @@ private:
 
 public:
     ChessRules();
-    ~ChessRules();
+    bool in_check(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state);
+    bool check_mate(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state);
+    bool check_stalemate(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state);
+    bool piece_movement(std::string piece, int cur_coords, int dest_coords);
+    bool check_en_passant(bool is_black, int cur_coords, int dest_coords);
+    bool check_promotion(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state);
+    bool check_castling(bool is_black, std::string side, std::unordered_map<int, std::vector<std::string>> board_state);
+    bool block_check(bool is_black, std::string side, std::unordered_map<int, std::vector<std::string>> board_state);
 };
 
 #endif //CHESS_RULES_HPP
