ea60f6f322bff4903bc6d51004bb96f6b62e1708
BRReed <b.r.reed@gmail.com>
2022-09-12 21:25:28 -0700

    skeleton methods for chessrules class file
    Signed-off-by: BRReed <b.r.reed@gmail.com>

--- Sources/ChessRules.cpp
+++ Sources/ChessRules.cpp
@@ -1,6 +1,51 @@
 #include "ChessRules.hpp"
 
+
+
 ChessRules::ChessRules()
 {
-    
+
+}
+
+bool ChessRules::block_check(bool is_black, std::string side, std::unordered_map<int, 
+                             std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+
+bool ChessRules::check_castling(bool is_black, std::string side, std::unordered_map<int, 
+                                std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+bool ChessRules::check_en_passant(bool is_black, int cur_coords, int dest_coords)
+{
+    return false;
+}
+
+bool ChessRules::check_mate(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+bool ChessRules::check_promotion(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+bool ChessRules::check_stalemate(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+bool ChessRules::in_check(bool is_black, std::unordered_map<int, std::vector<std::string>> board_state)
+{
+    return false;
+}
+
+bool ChessRules::piece_movement(std::string piece, int cur_coords, int dest_coords)
+{
+    return false;
 }
